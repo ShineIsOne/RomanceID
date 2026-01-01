c6814b1b941341af92660adfbc230ceab5fd8b47
Shine Is One <ikhsangunawan2801@gmail.com>
2026-01-01 19:27:13 +0700

    444

--- data.js
+++ data.js
@@ -26,7 +26,7 @@ const mangaList = [
             "Slice of Life"
         ],
         "status": "Ongoing",
-        "latestChapter": "Vol.8 Ch.99.5",
+        "latestChapter": "Vol.9 Ch.103",
         "link": "https://mangadex.org/title/51c1e7cc-23f4-4a08-a961-c1ab75e2bb33/kao-ni-denai-kashiwada-san-to-kao-ni-deru-oota-kun",
         "synopsis": "Kenalkan Kashiwada-san, siswi SMP yang wajahnya tetap datar meski sering diganggu. Dan Oota-kun, yang tidak bisa menyembunyikan ekspresi wajahnya, adalah kebalikan dari Kashiwada-san, tapi entah kenapa dia terus mengusilinya, meski tahu itu bukan ide bagus. Meski mereka sangat bertolak belakang, akankah rasa cinta tumbuh di antara mereka?"
     },
