c1c06b3810e0482e7e320b2189856c7634ecf076
Shine Is One <ikhsangunawan2801@gmail.com>
2026-01-08 21:07:33 +0700

    asdadadsggg

--- data.js
+++ data.js
@@ -71,7 +71,7 @@ const mangaList = [
             "School"
         ],
         "status": "Ongoing",
-        "latestChapter": "Vol.7 Ch.88",
+        "latestChapter": "Vol.7 Ch.88 | Up Next 16 Jan 2026",
         "link": "https://mangadex.org/title/955c730a-9304-4f82-ae96-af48ff5eaa0c/kurai-anoko-to-shitai-koto",
         "synopsis": "Siswa kelas 2 SMA, Tabata Ren, bertekad untuk tidak menjadi penyendiri mulai semester baru. Orang pertama yang ia ajak bicara demi mencari teman adalah seorang gadis yang terlihat suram dan sedikit menakutkan\u2026 tapi entah kenapa, matanya tak bisa lepas darinya, Sasaki Touko.  \nSebuah komedi tentang usaha lepas dari kesendirian bersama gadis pemalu nan menggemaskan!"
     },
@@ -86,7 +86,7 @@ const mangaList = [
             "Office Workers"
         ],
         "status": "Ongoing",
-        "latestChapter": "Vol.8 Ch.56",
+        "latestChapter": "Vol.8 Ch.56 | Up Next 23 Jan 2026",
         "link": "https://mangadex.org/title/baa95345-24fb-47a9-83e9-434ff671f968/super-no-ura-de-yani-suu-futari",
         "synopsis": "Sasaki, seorang pegawai kantoran yang kelelahan karena terlalu banyak kerja, menjalani hari-hari penuh stres hanya demi satu hal yang jadi cahaya di ujung terowongan: Yamada, kasir ceria yang selalu menyapanya di minimarket favoritnya.\nSuatu hari, Sasaki menghabiskan waktu lebih lama dari biasanya dalam rapat kantor, dan saat ia tiba di minimarket, Yamada sudah selesai shift dan pulang. Namun, saat hendak pergi, ia melihat seorang wanita muda bertindik sedang merokok di belakang toko?"
     }
