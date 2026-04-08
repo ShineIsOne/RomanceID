58352fd1c9e0143d7118bc2c8f5aae93350e22b5
Shine Is One <ikhsangunawan2801@gmail.com>
2026-04-08 19:24:05 +0700

    update

--- data.js
+++ data.js
@@ -296,5 +296,24 @@ const mangaList = [
     ],
     "type": "Order",
     "id": 1773560320543
+  },
+  {
+    "title": "Gal Shinkan wa Rizare ga Darui",
+    "image": "img/63b725fc-39f2-45ed-9aa3-1c8a685f54a1.webp",
+    "genres": [
+      "Comedy",
+      "Fantasy",
+      "Romance"
+    ],
+    "status": "Ongoing",
+    "latestChapter": "Vol.1 Ch.2 | Next Update ???",
+    "link": "https://mangadex.org/title/c6886f02-c7e5-41eb-aa35-c78bbd3bc757/gyaru-shinkan-wa-rizare-ga-darui",
+    "synopsis": "(IDN)\nUntuk mengalahkan Raja Iblis, seorang pahlawan terus bertarung sendirian berulang kali. Meski sudah mati berkali-kali, dia tetap maju tanpa ragu. Sebenarnya, dia melakukan semua itu karena ingin bertemu dan mengobrol berdua dengan seorang pendeta gyaru yang bisa menghidupkannya kembali. Bahkan mati pun tidak masalah baginya, selama dia bisa bertemu dengannya lagi.\n\n(ENG)\nTo defeat the Demon King, a hero continues to fight alone over and over again. Even though he dies countless times, he still pushes forward recklessly. In truth, he only does it because he wants to meet and spend time talking one-on-one with a gyaru priestess who can bring him back to life. Even dying doesn’t matter to him, as long as he can see her again.",
+    "lang": [
+      "id",
+      "en"
+    ],
+    "type": "TL",
+    "id": 1775650973058
   }
 ];
