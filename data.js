ca34317f67350b83c9034f5605985d1d0117acb8
Shine Is One <ikhsangunawan2801@gmail.com>
2026-04-16 09:41:33 +0700

    update

--- data.js
+++ data.js
@@ -1,5 +1,24 @@
 const mangaList = [
   {
+    "title": "Gyaru Shinkan wa Rizare ga Darui",
+    "image": "img/63b725fc-39f2-45ed-9aa3-1c8a685f54a1.webp",
+    "genres": [
+      "Comedy",
+      "Fantasy",
+      "Romance"
+    ],
+    "status": "Ongoing",
+    "latestChapter": "Vol.1 Ch.13 | Next Update 30 April 2026",
+    "link": "https://mangadex.org/title/c6886f02-c7e5-41eb-aa35-c78bbd3bc757/gyaru-shinkan-wa-rizare-ga-darui",
+    "synopsis": "(IDN)\nUntuk mengalahkan Raja Iblis, seorang pahlawan terus bertarung sendirian berulang kali. Meski sudah mati berkali-kali, dia tetap maju tanpa ragu. Sebenarnya, dia melakukan semua itu karena ingin bertemu dan mengobrol berdua dengan seorang pendeta gyaru yang bisa menghidupkannya kembali. Bahkan mati pun tidak masalah baginya, selama dia bisa bertemu dengannya lagi.\n\n(ENG)\nTo defeat the Demon King, a hero continues to fight alone over and over again. Even though he dies countless times, he still pushes forward recklessly. In truth, he only does it because he wants to meet and spend time talking one-on-one with a gyaru priestess who can bring him back to life. Even dying doesn’t matter to him, as long as he can see her again.",
+    "lang": [
+      "id",
+      "en"
+    ],
+    "type": "TL",
+    "id": 1775650973058
+  },
+  {
     "title": "Ore ga Inai to Sugu Chomeru Hakanagi Kasumi",
     "image": "img/9f637a7f-2ce1-4bc2-8329-b83733c780f9.jpg",
     "genres": [
@@ -77,25 +96,6 @@ const mangaList = [
     ],
     "type": "Order",
     "id": 1776053865381
-  },
-  {
-    "title": "Gyaru Shinkan wa Rizare ga Darui",
-    "image": "img/63b725fc-39f2-45ed-9aa3-1c8a685f54a1.webp",
-    "genres": [
-      "Comedy",
-      "Fantasy",
-      "Romance"
-    ],
-    "status": "Ongoing",
-    "latestChapter": "Vol.1 Ch.12 | Next Update 16 April 2026",
-    "link": "https://mangadex.org/title/c6886f02-c7e5-41eb-aa35-c78bbd3bc757/gyaru-shinkan-wa-rizare-ga-darui",
-    "synopsis": "(IDN)\nUntuk mengalahkan Raja Iblis, seorang pahlawan terus bertarung sendirian berulang kali. Meski sudah mati berkali-kali, dia tetap maju tanpa ragu. Sebenarnya, dia melakukan semua itu karena ingin bertemu dan mengobrol berdua dengan seorang pendeta gyaru yang bisa menghidupkannya kembali. Bahkan mati pun tidak masalah baginya, selama dia bisa bertemu dengannya lagi.\n\n(ENG)\nTo defeat the Demon King, a hero continues to fight alone over and over again. Even though he dies countless times, he still pushes forward recklessly. In truth, he only does it because he wants to meet and spend time talking one-on-one with a gyaru priestess who can bring him back to life. Even dying doesn’t matter to him, as long as he can see her again.",
-    "lang": [
-      "id",
-      "en"
-    ],
-    "type": "TL",
-    "id": 1775650973058
   },
   {
     "title": "Kurai Anoko to Shitai Koto",
